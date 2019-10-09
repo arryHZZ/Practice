4773f35dac8c1a7504a115f93fc89f93e87b1c80
arryHZZ <hanzhangzhe0519@163.com>
2019-10-09 20:39:14 +0800

    判断数组中的元素是否单调

---
+++ test_10.9/test_10.9/源.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+bool isMonotonic(int* A, int ASize){
+	int count1 = 1;
+	int count2 = 1;
+	int i = 0;
+	for (i; i < ASize - 1; i++)
+	{
+		if (A[i]>A[i + 1])
+		{
+			count1++;
+		}
+		else if (A[i]<A[i + 1])
+		{
+			count2++;
+		}
+		else
+		{
+			count1++;
+			count2++;
+		}
+	}
+	if (count1 == ASize || count2 == ASize)
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 4, 5 };
+	if (isMonotonic(arr, sizeof(arr) / sizeof(arr[0])))
+		printf("true\n");
+	else
+		printf("fasle");
+	return 0;
+}
